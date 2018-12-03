adf6e0c36f62a9626f2ac7403f9d0936e4cd9ba0
maartenvanderhoef <maarten@blinkist.com>
2018-12-03 20:47:31 +0100

    algolia search

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,10 +1,18 @@
 module.exports = {
   title : '.. ECS made easy',
   description : 'Home of Terraform Airship',
+  ga : 'UA-129919577-1',
   markdown : {
     extendMarkdown : md => { md.use(require('markdown-it-task-lists')) },
   },
   themeConfig : {
+    algolia : {
+      apiKey : '2bba00c0d0525534a021b4f04da510bf',
+      indexName : 'airship_terraform',
+      algoliaOptions : {
+        hitsPerPage : 10,
+      },
+    },
     displayAllHeaders : true,
     logo : '/airship.png',
     nav : [
@@ -45,7 +53,4 @@ module.exports = {
     repo : 'blinkist/terraform-aws-airship-ecs-service',
     editLinks : true,
   },
-  plugins : [
-    [ '@vuepress/search', {searchMaxSuggestions : 10} ],
-  ],
 }
